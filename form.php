c00a93efb589223e6743b33a903191e0e2868afd
Soufiane Aït <89341670+soufcod3@users.noreply.github.com>
2021-10-05 18:12:15 +0200

    Update form.php

--- form.php
+++ form.php
@@ -2,22 +2,22 @@
 <form  action="thanks.php"  method="post">
     <div>
       <label  for="nom">Nom :</label>
-      <input  type="text"  id="nom"  name="user_lastname" >
+      <input  type="text"  id="nom"  name="user_lastname" required>
     <div>
       <label  for="prenom">Prénom :</label>
-      <input  type="text"  id="prenom"  name="user_firstname" >
+      <input  type="text"  id="prenom"  name="user_firstname" required>
     </div>
     <div>
       <label  for="courriel">Courriel :</label>
-        <input  type="text"  id="courriel"  name="user_email" >
+        <input  type="email"  id="courriel"  name="user_email" required>
     </div>
     <div>
       <label  for="phone">Téléphone :</label>
-        <input  type="phone"  id="phone"  name="user_phone" >
+        <input  type="phone"  id="phone"  name="user_phone" required>
     </div>
     <div>
       <label  for="sujet">Vous nous contactez pour :</label>
-        <select id="subject" name="user_subject" >
+        <select id="subject" name="user_subject" required>
             <option value="">--Choisissez--</option>
             <option value="l'épisode covid">Supprimer l'épisode Covid de la mémoire de la population mondiale</option>
             <option value="votre futur job">Vous trouver le job de vos rêves</option>
@@ -26,7 +26,7 @@
     </div>
     <div>
       <label  for="message">Message :</label>
-      <textarea  type="textarea" id="message"  name="user_message" ></textarea>
+      <textarea  type="textarea" id="message"  name="user_message" required></textarea>
     </div>
     <div  class="button">
       <button  type="submit">Envoyer votre message</button>
